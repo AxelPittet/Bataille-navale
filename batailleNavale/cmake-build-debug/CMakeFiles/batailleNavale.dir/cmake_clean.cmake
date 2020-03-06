f0d91a562e71aece14b7d3fce4b870981b0ba498
Axwells <Axel.PITTET@cpnv.ch>
2020-03-06 10:57:35 +0100

    Projet Clion
    Documents de base du projets

---
+++ batailleNavale/cmake-build-debug/CMakeFiles/batailleNavale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/batailleNavale.dir/main.c.obj"
+  "batailleNavale.exe"
+  "batailleNavale.exe.manifest"
+  "batailleNavale.pdb"
+  "libbatailleNavale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/batailleNavale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

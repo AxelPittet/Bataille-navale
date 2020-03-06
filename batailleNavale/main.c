6a74f29191537674d3b989e9f6e380d8c94af2d0
Axwells <Axel.PITTET@cpnv.ch>
2020-03-06 12:01:20 +0100

    Règles + Quitter
    Les fonctionnalités règles et quitter sont maintenant fonctionnelles

--- batailleNavale/main.c
+++ batailleNavale/main.c
@@ -7,29 +7,66 @@
 
 int choixMenu;
 
+void clear() {
+    system("cls");
+}
+
+void pause() {
+    system("pause");
+}
+
 void afficherTitre() {
-    printf("                     .:°       | |\n");
-    printf("                 ___|____      | |\n");
-    printf("                / ° ° ° /      | |\n");
-    printf("         ---------------------------------\n");
-    printf("        <=========Bataille Navale=========>\n");
-    printf("          <=============================>\n");
-    printf("            <=========================>\n\n");
+    printf("                       .:°       | |\n");
+    printf("                   ___|____      | |\n");
+    printf("                  / ° ° ° /      | |\n");
+    printf("           ---------------------------------\n");
+    printf("          <=========Bataille Navale=========>\n");
+    printf("            <=============================>\n");
+    printf("              <=========================>\n\n");
 
 }
 
 void afficherChoixMenu() {
-    printf("                  1 - Jouer\n");
-    printf("                  2 - Règles\n");
-    printf("                  3 - Scores\n");
-    printf("                  4 - Quitter\n\n");
+    printf("                     1 - Jouer\n");
+    printf("                     2 - Règles\n");
+    printf("                     3 - Scores\n");
+    printf("                     4 - Quitter\n\n");
 }
 
 void demanderChoixMenu() {
-    printf("                     Choix: ");
+    printf("                       Choix: ");
 
     fflush(stdin);
     scanf("%d", &choixMenu);
+}
+
+void quitter() {
+    clear();
+
+    afficherTitre();
+
+    printf("\n                À une prochaine fois!\n\n\n\n");
+    printf("     ");
+    pause();
+}
+
+void regles() {
+    clear();
+
+    printf("                        Règles\n\n");
+    printf(" Chaque joueur possède:      Un bateau de 5 cases\n");
+    printf("                             Un bateau de 4 cases\n");
+    printf("                             Deux bateaux de 4 cases\n");
+    printf("                             Un bateau de 2 cases\n\n");
+
+    printf(" Chacun son tour chaque joueur tire un obus sur une\n case de la carte adverse.\n");
+    printf(" Un bateau est touché quand un obus aterrit sur une\n des cases du bateau.\n");
+    printf(" Un bateau est coulé quand toutes ses cases sont touchées.\n");
+    printf(" Une partie se termine quand un joueur coule tout les\n bateaux adverses\n\n");
+
+    printf("                  ");
+
+    pause();
 }
 
 int main() {
@@ -38,7 +75,7 @@ int main() {
 
     do {
 
-        system("cls");
+        clear();
 
         afficherTitre();
 
@@ -46,10 +83,23 @@ int main() {
 
         demanderChoixMenu();
 
-    } while (choixMenu != 4);
+        switch(choixMenu) {
+            case 1:
 
-    //a degager apres
-    system("pause");
+                break;
+            case 2:
+                regles();
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+                quitter();
+                break;
+        }
+
+    } while (choixMenu != 4);
 
     return 0;
 }

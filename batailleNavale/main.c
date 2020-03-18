d265b8fb849243abe1f8f889ab8d2d22aa6df0e5
Axwells <Axel.PITTET@cpnv.ch>
2020-03-18 19:46:05 +0100

    commmentaires

--- batailleNavale/main.c
+++ batailleNavale/main.c
@@ -1,10 +1,19 @@
+/**
+ * Libraries
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
 #include <math.h>
 
+/**
+ * Permet d'afficher les caractères spéciaux
+ */
 #pragma set_character_execution
 
+/**
+ * Constantes
+ */
 #define NOMBRE_COLONNES 10
 #define NOMBRE_LIGNES 10
 
@@ -35,17 +44,29 @@
 #define LIGNE1_BATEAU2 3
 #define LIGNE2_BATEAU2 4
 
+/**
+ * Initioalisations
+ */
 char carte[NOMBRE_COLONNES][NOMBRE_LIGNES], cibleLigne, b;
 int choixMenu, cibleColonne, bateau5, bateau4, bateau3A, bateau3B, bateau2, win;
 
+/**
+ * Fonction servant à réinitialiser la console
+ */
 void clear() {
     system("cls");
 }
 
+/**
+ * Fonction servant à éviter une fermeture brusque
+ */
 void pause() {
     system("pause");
 }
 
+/**
+ * Fonction servant à afficher le titre
+ */
 void afficherTitre() {
     printf("                       .:°       | |\n");
     printf("                   ___|____      | |\n");
@@ -57,6 +78,9 @@ void afficherTitre() {
 
 }
 
+/**
+ * Fonction servant à afficher les différents choix du menu
+ */
 void afficherChoixMenu() {
     printf("                     1 - Jouer\n");
     printf("                     2 - Règles\n");
@@ -64,6 +88,9 @@ void afficherChoixMenu() {
     printf("                     4 - Quitter\n\n");
 }
 
+/**
+ * Fonction servant a demander à l'utilisateur quelle fonctionnalité du menu il veut utiliser
+ */
 void demanderChoixMenu() {
     printf("                       Choix: ");
 
@@ -71,6 +98,9 @@ void demanderChoixMenu() {
     scanf("%d", &choixMenu);
 }
 
+/**
+ * Fonction servant à afficher les élements nécessaires lorsque le choix était de quitter
+ */
 void quitter() {
     clear();
 
@@ -81,6 +111,9 @@ void quitter() {
     pause();
 }
 
+/**
+ * Fonction servant à afficher les règles
+ */
 void regles() {
     clear();
 
@@ -100,7 +133,11 @@ void regles() {
     pause();
 }
 
+/**
+ * Fonction servant à donner les valeurs nécessaires à la carte
+ */
 void remplirCarte() {
+
     for (int i = 0; i < 10; i++) {
         for (int a = 0; a < 10; a++) {
             carte[i][a] = ' ';
@@ -108,6 +145,9 @@ void remplirCarte() {
     }
 }
 
+/**
+ * Fonction servant à afficher la carte
+ */
 void afficherCarte() {
     printf("    1  2  3  4  5  6  7  8  9  10\n");
 
@@ -120,12 +160,18 @@ void afficherCarte() {
     }
 }
 
+/**
+ * Fonction servant à afficher les indicatins de jeu
+ */
 void indicationsJeu() {
-    printf("  O = tir dans l'eau\n");
+    printf("  - = tir dans l'eau\n");
     printf("  X = tir touché\n");
     printf("  C = bateau coulé\n\n\n");
 }
 
+/**
+ * Fonction servant à demander où l'utilisateur veut tirer
+ */
 void tirer() {
     printf("   Colonne de la cible : ");
     fflush(stdin);
@@ -136,7 +182,11 @@ void tirer() {
     scanf("%c", &cibleLigne);
 }
 
+/**
+ * Fonction servant à convertir les donnés reçues afin de pouvoir les utiliser dans le tableau (carte)
+ */
 void convertirTir() {
+
     if ((cibleLigne >= 'a') && (cibleLigne <= 'j')) {
         cibleLigne -= 97;
     } else if ((cibleLigne >= 'A') && (cibleLigne <= 'J')) {
@@ -145,7 +195,13 @@ void convertirTir() {
     cibleColonne -= 1;
 }
 
+/**
+ * Fonction servant à savoir si le tir touche un bateau, le coule ou est dans l'eau
+ */
 void changerValeurCarte() {
+    /**
+     * Conditions à respecter pour qu'un bateau soit touché
+     */
     if (((cibleColonne == COLONNE_BATEAU5) && ((cibleLigne == LIGNE1_BATEAU5) || (cibleLigne == LIGNE2_BATEAU5)
                                                || (cibleLigne == LIGNE3_BATEAU5) || (cibleLigne == LIGNE4_BATEAU5) ||
                                                (cibleLigne == LIGNE5_BATEAU5)))
@@ -162,6 +218,9 @@ void changerValeurCarte() {
 
         carte[cibleColonne][cibleLigne] = 'X';
 
+        /**
+         * Conditions à respecter pour que le bateau de 5 cases soit coulé
+         */
         if ((carte[COLONNE_BATEAU5][LIGNE1_BATEAU5] == 'X') && (carte[COLONNE_BATEAU5][LIGNE2_BATEAU5] == 'X')
             && (carte[COLONNE_BATEAU5][LIGNE3_BATEAU5] == 'X') && (carte[COLONNE_BATEAU5][LIGNE4_BATEAU5] == 'X')
             && (carte[COLONNE_BATEAU5][LIGNE5_BATEAU5] == 'X')) {
@@ -172,8 +231,14 @@ void changerValeurCarte() {
 
             printf("   Coulé\n\n");
 
+            /**
+             * Permet de savoir que le bateau de 5 cases est coulé
+             */
             bateau5 = 1;
 
+        /**
+        * Conditions à respecter pour que le bateau de 4 cases soit coulé
+        */
         } else if ((carte[COLONNE1_BATEAU4][LIGNE_BATEAU4] == 'X') && (carte[COLONNE2_BATEAU4][LIGNE_BATEAU4] == 'X')
                    && (carte[COLONNE3_BATEAU4][LIGNE_BATEAU4] == 'X') &&
                    (carte[COLONNE4_BATEAU4][LIGNE_BATEAU4] == 'X')) {
@@ -184,8 +249,14 @@ void changerValeurCarte() {
 
             printf("   Coulé\n\n");
 
+            /**
+            * Permet de savoir que le bateau de 4 cases est coulé
+            */
             bateau4 = 1;
 
+        /**
+        * Conditions à respecter pour que le premier bateau de 3 cases soit coulé
+        */
         } else if ((carte[COLONNE1_BATEAU3A][LIGNE_BATEAU3A] == 'X') &&
                    (carte[COLONNE2_BATEAU3A][LIGNE_BATEAU3A] == 'X')
                    && (carte[COLONNE3_BATEAU3A][LIGNE_BATEAU3A] == 'X')) {
@@ -196,8 +267,14 @@ void changerValeurCarte() {
 
             printf("   Coulé\n\n");
 
+            /**
+            * Permet de savoir que le premier bateau de 3 cases est coulé
+            */
             bateau3A = 1;
 
+        /**
+        * Conditions à respecter pour que le deuxième bateau de 3 cases soit coulé
+        */
         } else if ((carte[COLONNE1_BATEAU3B][LIGNE_BATEAU3B] == 'X') &&
                    (carte[COLONNE2_BATEAU3B][LIGNE_BATEAU3B] == 'X') &&
                    (carte[COLONNE3_BATEAU3B][LIGNE_BATEAU3B] == 'X')) {
@@ -208,8 +285,14 @@ void changerValeurCarte() {
 
             printf("   Coulé\n\n");
 
+            /**
+            * Permet de savoir que le dexième bateau de 3 cases est coulé
+            */
             bateau3B = 1;
 
+        /**
+        * Conditions à respecter pour que le bateau de 2 cases soit coulé
+        */
         } else if ((carte[COLONNE_BATEAU2][LIGNE1_BATEAU2] == 'X') && (carte[COLONNE_BATEAU2][LIGNE2_BATEAU2] == 'X')) {
 
             for (int i = 3; i < 5; i++) {
@@ -218,6 +301,9 @@ void changerValeurCarte() {
 
             printf("   Coulé !\n\n");
 
+            /**
+            * Permet de savoir que le bateau de 2 cases est coulé
+            */
             bateau2 = 1;
 
         } else {
@@ -232,20 +318,32 @@ void changerValeurCarte() {
     printf("   ");
 }
 
+/**
+ * Fonction servant à savoir si un tir est valide
+ */
 void verfierTir() {
     if ((cibleColonne < 0) || (cibleColonne > 9) || (cibleLigne < 0) || (cibleLigne > 9)) {
-        printf("   Tir non valide\n\n");
-    } else {
+            printf("   Tir non valide\n\n");
+        } else {
         changerValeurCarte();
     }
 }
 
+/**
+ * Fonction servant à vérifier si les conditions de victoire sont remplies
+ */
 void conditionsVictoire() {
     if ((bateau5 == 1) && (bateau4 == 1) && (bateau3A == 1) && (bateau3B == 1) && (bateau2 == 1)) {
+        /**
+         * Permet de savoir si les conditions sont remplies
+         */
         win = 1;
     }
 }
 
+/**
+ * Fonction contenant d'autres fonctions qui réunies permettent de jouer à la bataille navale
+ */
 void jouer() {
     remplirCarte();
 
@@ -268,26 +366,35 @@ void jouer() {
     } while (win != 1);
     clear();
 
+    /**
+     * Affichage du message de victoire
+     */
     printf(".______   .______           ___      ____    ____   ______      .___________. __    __          ___"
-           "           _______      ___        _______ .__   __.  _______     __   __   __  \n"
-           "|   _  \\  |   _  \\         /   \\     \\   \\  /   /  /  __  \\     |           ||  |  |  |        /   \\"
-           "         /  _____|    /   \\      /  _____||  \\ |  | |   ____|   |  | |  | |  | \n"
-           "|  |_)  | |  |_)  |       /  ^  \\     \\   \\/   /  |  |  |  |    `---|  |----`|  |  |  |       /  ^  \\"
-           "       |  |  __     /  ^  \\    |  |  __  |   \\|  | |  |__      |  | |  | |  | \n"
-           "|   _  <  |      /       /  /_\\  \\     \\      /   |  |  |  |        |  |     |  |  |  |      /  /_\\"
-           "  \\      |  | |_ |   /  /_\\  \\   |  | |_ | |  . `  | |   __|     |  | |  | |  | \n"
-           "|  |_)  | |  |\\  \\----. /  _____  \\     \\    /    |  `--'  |        |  |     |  `--'  |     /  _____"
-           "  \\     |  |__| |  /  _____  \\  |  |__| | |  |\\   | |  |____    |__| |__| |__| \n"
-           "|______/  | _| `._____|/__/     \\__\\     \\__/      \\______/         |__|      \\______/     /__/"
-           "     \\__\\     \\______| /__/     \\__\\  \\______| |__| \\__| |_______|   (__) (__) (__) \n\n\n\n\n\n\n");
+       "           _______      ___        _______ .__   __.  _______     __   __   __  \n"
+    "|   _  \\  |   _  \\         /   \\     \\   \\  /   /  /  __  \\     |           ||  |  |  |        /   \\"
+    "         /  _____|    /   \\      /  _____||  \\ |  | |   ____|   |  | |  | |  | \n"
+    "|  |_)  | |  |_)  |       /  ^  \\     \\   \\/   /  |  |  |  |    `---|  |----`|  |  |  |       /  ^  \\"
+    "       |  |  __     /  ^  \\    |  |  __  |   \\|  | |  |__      |  | |  | |  | \n"
+    "|   _  <  |      /       /  /_\\  \\     \\      /   |  |  |  |        |  |     |  |  |  |      /  /_\\"
+    "  \\      |  | |_ |   /  /_\\  \\   |  | |_ | |  . `  | |   __|     |  | |  | |  | \n"
+    "|  |_)  | |  |\\  \\----. /  _____  \\     \\    /    |  `--'  |        |  |     |  `--'  |     /  _____"
+    "  \\     |  |__| |  /  _____  \\  |  |__| | |  |\\   | |  |____    |__| |__| |__| \n"
+    "|______/  | _| `._____|/__/     \\__\\     \\__/      \\______/         |__|      \\______/     /__/"
+    "     \\__\\     \\______| /__/     \\__\\  \\______| |__| \\__| |_______|   (__) (__) (__) \n\n\n\n\n\n\n");
 
     pause();
 }
 
 int main() {
 
+    /**
+     * permet d'afficher les caractères spéciaux
+     */
     SetConsoleOutputCP(65001);
 
+    /**
+     * Boucle principale revenant toujours au menu tant que le choix n'est pas de quitter
+     */
     do {
 
         clear();
@@ -298,6 +405,9 @@ int main() {
 
         demanderChoixMenu();
 
+        /**
+         * Switch contenant les différentes fonctions
+         */
         switch (choixMenu) {
             case 1:
                 jouer();
